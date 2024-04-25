1aa56c9cdb2dcafbf3a4b4c89f87c26216ab829a
mcc <andi.m.mcclure@gmail.com>
2024-04-25 10:34:05 -0400

    Load file once

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,52 @@
+use clap::Parser;
+use std::fs::File;
+use std::path::PathBuf;
+use minimp3::{Frame, Decoder};
+use tuple_utils::Append;
+
+#[derive(Parser)]
+struct Cli {
+    mp3: PathBuf,
+    //output: PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    // Decode all // FIXME: Streaming would be nice but then we can't get a priori length
+    let (sample_rate, channels, data) = {
+        let mut decoder =
+            Decoder::new(File::open(cli.mp3).expect("Could not open file"));
+        let mut sample_rate_channels : Option<(i32, usize)> = Default::default();
+        let mut float_data: Vec<f32> = Default::default();
+
+        loop {
+            match decoder.next_frame() {
+                Ok(Frame {
+                    data,
+                    sample_rate,
+                    channels,
+                    ..
+                }) => {
+                    match (sample_rate_channels) {
+                        None => {
+                            sample_rate_channels = Some((sample_rate, channels))
+                        }
+                        Some(sample_rate_channels) => {
+                            std::assert!(sample_rate_channels == (sample_rate, channels), "Sample rate or channels changed? At sample {}, was {sample_rate_channels:?}, now ({sample_rate}, {channels})", float_data.len());
+                        }
+                    }
+                    for sample in data {
+                        float_data.push(sample as f32/std::i16::MAX as f32);
+                    }
+                },
+                Err(minimp3::Error::Eof) => break,
+                Err(e) => panic!("{:?}", e),
+            }
+        }
+
+        sample_rate_channels.expect("No frames in mp3?").append(float_data)
+    };
+
+    println!("{sample_rate}hz, {channels} channels");
 }
